0a30a559fcc53792d77a8efc55d19b681bcb22da
Tobias Klemmer <tobias@klemmer.info>
2021-01-03 21:27:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Creating an .ico file
+
+ffmpeg -i img.png img.ico
---
+++ src/pages/_app.js
@@ -0,0 +1,36 @@
+import Head from "next/head"
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"
+                />
+                <meta name="description" content="Blog von Tobias Klemmer" />
+                <meta name="keywords" content="Blog, Tech, Learn" />
+                <title>klemmer.info</title>
+
+                <link rel="manifest" href="/manifest.json" />
+                <link
+                    href="/icons/favicon-16x16.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                />
+                <link
+                    href="/icons/favicon-32x32.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                />
+                <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+                <meta name="theme-color" content="#4F46E5" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
+}
